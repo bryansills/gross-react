1106519158e5798767bc192782e1738e4888d67d
Bryan Sills <bryan.sills@isobar.com>
2017-09-29 10:44:25 -0500

    added word input

--- Screens/ChooseScreen.js
+++ Screens/ChooseScreen.js
@@ -6,24 +6,48 @@ import {
     Text,
     View
 } from 'react-native';
+import { Keyboard } from '../Views';
 
 export default class ChooseScreen extends Component {
+
+    constructor(props) {
+        super(props);
+
+        let word = '';
+
+        this.state = {word: word};
+    }
+
     static navigationOptions = {
         title: 'Choose A Word',
     };
+
     render() {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
-                    Welcome to React Native!
+                    Your word is:
+                </Text>
+                <Text style={styles.welcome}>
+                    {this.state.word}
                 </Text>
                 <Button 
-                    onPress={() => navigate('Play', {word: 'BLARGGGGGG'})}
+                    onPress={() => navigate('Play', {word: this.state.word})}
                     title="Let's Play!" />
+                <Keyboard usedLetters={[]} onLetterPress={this.letterPressed}/>
             </View>
         );
     }
+    
+    letterPressed = (letter) => {
+        this.setState((prevState) => {
+            let newState = {...prevState};
+            newState.word += letter;
+
+            return newState
+        });
+    };        
 }
 
 const styles = StyleSheet.create({
